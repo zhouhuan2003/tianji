93059a270044ad382829548dda39c2c7a199e1f4
虎哥 <huyi0612@163.com>
2022-12-05 11:53:31 +0800

    实现与我的课表有关的接口

---
+++ tj-learning/src/main/java/com/tianji/learning/mapper/LearningLessonMapper.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.mapper;
+
+import com.tianji.learning.domain.po.LearningLesson;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 学生课程表 Mapper 接口
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2022-12-02
+ */
+public interface LearningLessonMapper extends BaseMapper<LearningLesson> {
+
+}
